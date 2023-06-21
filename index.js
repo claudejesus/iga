4509a9148c9a5fc6a98d790af5015479583ce6da
claudejesus <niyigabaclaujesus@gmail.com>
2023-06-21 15:46:43 +0200

    update

---
+++ index.js
@@ -0,0 +1,162 @@
+import React from "react";
+import styles from "./Index.module.scss";
+
+const Index = () => {
+  return (
+    <div className={styles.index}>
+      <div className={styles.flexWrapperTwo}>
+        <img
+          alt=""
+          className={styles.logoRemovebgPreview1}
+          src="https://static.overlay-tech.com/assets/0c86a30c-1b17-438a-ae95-1ceda31f3525.png"
+        />
+        <div className={styles.group2}>
+          <img
+            alt=""
+            className={styles.vector}
+            src="https://static.overlay-tech.com/assets/6b6c88e8-cf4b-4ba6-8cd4-1f8100a74d65.svg"
+          />
+          <p className={styles.english}>English</p>
+          <img
+            alt=""
+            className={styles.vector1}
+            src="https://static.overlay-tech.com/assets/b95a662e-8fa6-4818-a7d7-e9cde4b044b6.svg"
+          />
+        </div>
+      </div>
+      <p
+        className={styles.joinMillionsOfUsersToBuyAndSellA}
+      >
+        Join millions of users to buy and sell anything in
+        RWANDA
+      </p>
+      <div className={styles.flexWrapperThree}>
+        <div className={styles.group7}>
+          <p className={styles.hotOffersMusanze}>
+            Hot Offers Musanze
+          </p>
+          <p className={styles.apartmentForSaleInMusanze}>
+            Apartment for sale in Musanze
+          </p>
+          <p className={styles.jobsInMusanze}>
+            Jobs in Musanze
+          </p>
+          <p className={styles.usedCarForSaleInMusanze}>
+            Used car for sale in Musanze
+          </p>
+          <p className={styles.apartmentForRentInMusanze}>
+            Apartment for rent in Musanze
+          </p>
+        </div>
+        <div className={styles.group8}>
+          <p className={styles.hotOffersKigaliCity}>
+            Hot Offers Kigali City
+          </p>
+          <p
+            className={styles.apartmentForSaleInKigaliCity}
+          >
+            Apartment for sale in Kigali City
+          </p>
+          <p className={styles.jobsInKigaliCity}>
+            Jobs in Kigali City
+          </p>
+          <p className={styles.usedCarForSaleInKigaliCity}>
+            Used car for sale in Kigali City
+          </p>
+          <p
+            className={styles.apartmentForRentInKigaliCity}
+          >
+            Apartment for rent in Kigali City
+          </p>
+        </div>
+        <div className={styles.group7}>
+          <p className={styles.hotOffersKigaliCity}>
+            Hot Offers Muhanga
+          </p>
+          <p
+            className={styles.apartmentForSaleInKigaliCity}
+          >
+            Apartment for sale in Muhanga
+          </p>
+          <p className={styles.jobsInKigaliCity}>
+            Jobs in Muhanga
+          </p>
+          <p className={styles.usedCarForSaleInKigaliCity}>
+            Used car for sale in Muhanga
+          </p>
+          <p
+            className={styles.apartmentForRentInKigaliCity}
+          >
+            Apartment for rent in Muhanga
+          </p>
+        </div>
+      </div>
+      <p
+        className={styles.hotoffersCom2023AllRightsReserve}
+      >
+        © hotoffers.com 2023, All Rights Reserved.
+      </p>
+      <div className={styles.relativeWrapperOne}>
+        <div className={styles.flexWrapperOne}>
+          <img
+            alt=""
+            className={styles.domeBusinessClassLoungeOrig1}
+            src="https://static.overlay-tech.com/assets/26affbf4-e104-430b-af69-3483226ca691.png"
+          />
+          <p
+            className={
+              styles.welcomeToTheRwanda1OnlineClassifi
+            }
+          >
+            Welcome to the Rwanda #1 <br />
+            online classifieds website!
+          </p>
+          <p
+            className={
+              styles.whichPartOfRwandaWouldYouLikeToE
+            }
+          >
+            Which part of Rwanda would you like to explore?
+          </p>
+        </div>
+        <div className={styles.group2Two}>
+          <p className={styles.hotOffersRubavu}>
+            Hot Offers
+            <br />
+            Rubavu
+          </p>
+        </div>
+        <div className={styles.group3}>
+          <p className={styles.hotOffersRubavu}>
+            Hot Offers
+            <br />
+            Muhanga
+          </p>
+        </div>
+        <div className={styles.group4}>
+          <p className={styles.hotOffersRubavu}>
+            Hot Offers
+            <br />
+            Kigali City
+          </p>
+        </div>
+        <div className={styles.group5}>
+          <p className={styles.hotOffersRubavu}>
+            Hot Offers
+            <br />
+            Huye
+          </p>
+        </div>
+        <div className={styles.group6}>
+          <p className={styles.hotOffersRubavu}>
+            Hot Offers
+            <br />
+            Musanze
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
